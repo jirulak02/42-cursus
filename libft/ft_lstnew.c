7015422f86c0c6eb5405207221454a5e2bf88b83
Jiri Simecek <jsimecek@c2r5s5.42prague.com>
2023-02-23 11:40:36 +0100

    started bonus

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsimecek <jsimecek@student.42prague.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 11:15:49 by jsimecek          #+#    #+#             */
+/*   Updated: 2023/02/23 11:33:21 by jsimecek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*l;
+
+	l = (t_list *)malloc(sizeof(t_list));
+	if (!l)
+		return (NULL);
+	l->content = content;
+	l->next = NULL;
+	return (l);
+}
