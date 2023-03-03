8f04f77d97774aa1d2d9a2f00c91bddea01141e3
Jiri Simecek <jsimecek@c2r5s5.42prague.com>
2023-03-03 14:31:46 +0100

    calloc fix

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jsimecek <jsimecek@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 10:05:50 by jsimecek          #+#    #+#             */
-/*   Updated: 2023/01/18 12:20:54 by jsimecek         ###   ########.fr       */
+/*   Updated: 2023/03/03 14:30:40 by jsimecek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	*ft_calloc(size_t nelem, size_t elsize)
 	void	*p;
 	size_t	i;
 
+	if (nelem == 0 || elsize == 0)
+	{
+		nelem = 1;
+		elsize = 1;
+	}
 	p = NULL;
 	array = malloc(nelem * elsize);
 	if (!array)
